3112dc5bf59d97f8f8d9f3719f3357fcb4e60d8d
Peyton Doyle <p6doyle@gmail.com>
2019-07-16 14:46:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,14 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["Pink","Beyonce", "Cher", "Madonna"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  arr = ["Pink","Beyonce", "Cher", "Madonna"]
+  arr << "Gaga"
 end
 
 def add_element_to_start_of_array(array, element)

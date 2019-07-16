1205b0a7c7c3ea5c09269a50b962a1a7727f6c29
Peyton Doyle <p6doyle@gmail.com>
2019-07-16 14:48:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,7 +17,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+    arr = ["Pink","Beyonce", "Cher", "Madonna", "arrays!"]
+    arr.pop
 end
 
 def remove_element_from_start_of_array(array)
